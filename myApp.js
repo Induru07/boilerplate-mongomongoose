fe106e7b6dabd2869e7de945cb7365697a245031
W.G.I.Adeesha <induruadeesha2356@gmail.com>
2025-12-03 20:48:58 +0000

    Restore Person schema and creation functions

--- myApp.js
+++ myApp.js
@@ -10,13 +10,53 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   });
 
 let Person;
+const Schema = mongoose.Schema;// create a person schema
+const personSchema = new Schema({// define the schema here
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+//create a model with the schema(the manufacturer of the document)
+Person = mongoose.model("Person", personSchema);
+/*in here  we create functions to make operations with the database 
+and Person model we created before*/
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  // create a person document
+  const person = new Person({
+    name: "John Doe",
+    age: 30,
+    favoriteFoods: ["pizza", "pasta" , "kankun"] 
+  })
+
+  // save the document to the database
+  person.save(function(err, data) {//we pass a callback function to handle the result
+
+    //handle Error(Node Convention)
+    if (err) {
+      return done(err);//If error,tell the system we failed
+    }
+    //Handle Success
+    //if no error, we pass null as the first argument and the saved data as the second
+    done(null, data);
+
+    
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  // create many people documents
+  Person.create(arrayOfPeople, function(err, people){
+    //standard Node error handling
+    if (err){
+      return done(err);
+    }
+    //success!
+    //'people' is an array of the created documents
+    done(null, people);
+  })
+
 };
 
 const findPeopleByName = (personName, done) => {
