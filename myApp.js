db8df76a60f679552f2a230ca027845254bec155
W.G.I.Adeesha <induruadeesha2356@gmail.com>
2025-12-05 18:50:36 +0000

    Update myApp.js and package-lock.json

--- myApp.js
+++ myApp.js
@@ -60,43 +60,156 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  // find people by name
+  Person.find({ name: personName}, function(err, personFound){
+    //standard error handling
+    if (err){
+      return done(err);
+    }
+    //success!
+    //'personFound' is an array of the found documents
+    done(null, personFound);
+  })
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  // find one person by favorite food
+  Person.findOne({favouriteFoods: food}, function(err,data){
+    //standard error handling
+    if (err){
+      return done(err);
+    }
+    //success!
+    //Note: 'data' is a single objct{name : ..., age: ..., favoriteFoods: [...]}
+    //we can access to name like data.name
+    //if no match is found 'data' is null
+    done(null, data);
+  })
 };
 
-const findPersonById = (personId, done) => {
-  done(null /*, data*/);
-};
+const findPersonById = ((personId, done) => {
+  // find person by ID
+  Person.findById(personId, function(err, data){
+    //standard error handling
+    if (err){
+      return done(err);
+    }
+    //success!
+    //'data' is the single documentmatching ID
+
+  done(null , data);
+});
+});
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  // find person by ID
+  Person.findById(personId, function(err, person){
+    if (err){
+      return done(err);
+    }
+    //add the food to the person's favoriteFoods array
+    person.favoriteFoods.push(foodToAdd);
+    //save the updated document
+    person.save(function(err, updatedPerson){
+      if (err){
+        return done(err);
+      }
+      //success!
+      done(null, updatedPerson);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
+  // find one person by name and update their age
+  //Patterrn : Model.findOneAndUpdate(conditions, update, options, callback)
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName }, //conditions
+    {age: ageToSet} ,//update
+    { new: true }, //options: return the updated document
+    function(err, updatedPerson){
+      if (err){
+        return done(err);
+      }
+      //success!
+      done(null, updatedPerson);
+    }
+  );
 };
 
+
+
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  /*Think?
+  1.Model? -> Person
+  2.Method? -> findByIdAndRemove
+  3.Arguments? -> personId, callback
+  4.Callback? -> function(err, data){...}
+  5.Error Handling? -> if(err) return done(err);
+  6.Success Handling? -> done(null, data);
+  7.What is data? -> the removed document
+  8.Return? -> done(null, data);
+  9.Final Code? -> see below
+  */
+
+  Person.findByIdAndDelete(personId, function(err, data){
+    if (err){
+      return done(err);
+    }
+    //success!
+    //'removedDoc' contains the object details of the person we just deleted.
+    //If the ID didn't exist, 'removedDoc' is null
+    done(null, data);
+  }
+  );
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
+  /*Think?
+  1.Model? -> Person
+  2.Method? -> .deleteMany()
+  3.Arguments? -> {name: nameToRemove}, callback
+  4.Callback? -> function(err, data){...}
+  5.Error Handling? -> if(err) return done(err);
+  6.Success Handling? -> done(null, data);
+  7.What is data? -> the result object containing info about the operation
+  8.Return? -> done(null, data);
+  9.Final Code? -> see below
+  */
 
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, function(err, data){
+    if (err){
+      return done(err);
+    }
+    //success!
+    //'data' contains info about the operation
+    //like number of documents deleted
+    done(null, data);
+  }
+  );
+  
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  //build the pipeline of query methods
+  Person.find({ favoriteFoods: foodToSearch }) //find people who like burrito
+    .sort({ name: 1 }) //sort by name ascending
+    .limit(2) //limit to 2 results
+    .select({ age: 0 }) //exclude age field
+    .exec(function(err, data){ //execute the query
+      if (err){
+        return done(err);
+      }
+      //success!
+      //'data' is the array of matching documents
+      done(null, data);
+    }
+    );
 };
 
 /** **Well Done !!**
